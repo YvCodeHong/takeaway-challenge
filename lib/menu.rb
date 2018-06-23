e6a84158003a49147f8d25f4cfc6037ec7e55ad2
Yvonne Tang <ytang604@gmail.com>
2018-06-23 23:18:04 +0100

    add list

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,9 +5,15 @@ class Menu
 attr_reader :price_list
 
  def initialize
-   @price_list
+   @price_list = {
+     "As closely linked as flesh and blood" => 3.49,
+     "Stupid chicken stew potato dry" => 3.60,
+     "Explodes the stomach" => 4.50,
+     "The peasant family stir_fries flesh for a short time" => 2.40
+   }
  end
 
  def price_list
+   @price_list
  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,8 +3,10 @@ require 'menu'
 describe Menu do
   let(:menu) { described_class.new }
 
-  it 'should list price of dishes' do
+  it 'should list price of chinglish_dishes' do
      menu = Menu.new
      expect(subject.price_list).to eq menu.price_list
     end
+
+  #quantities and orders
 end
